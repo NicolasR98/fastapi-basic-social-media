a8149a40696f89ddd6e0fdbe8930818cadaa2090
Nicolas Rios <dnicolas.rios98@gmail.com>
2023-08-14 20:59:47 +0200

    Add initial user endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 
-from . import schemas
+from . import schemas, utils
 from .sql_app import models
 from .sql_app.database import engine, get_db
 from .sql_app.models import Base
@@ -55,7 +55,7 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     if post_query.first() == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Post not found"
+            detail=f"Post with id {id} was not found"
         )
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
@@ -72,3 +72,24 @@ def delete_post(id: int, db: Session = Depends(get_db)):
         )
     post.delete(synchronize_session=False)
     db.commit()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user.password = utils.hash_password(user.password)
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def read_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {id} does not exist"
+        )
+    return user
