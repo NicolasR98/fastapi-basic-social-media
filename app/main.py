44aad34fe3dd7477b9bfc8f73e37e1ead3e675f6
Nicolas Rios <dnicolas.rios98@gmail.com>
2023-09-21 21:17:04 +0200

    Add initial config for CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, posts, users, vote
 
@@ -8,6 +9,19 @@ from .sql_app.models import Base
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(posts.router)
